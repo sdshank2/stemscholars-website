0ef8f2ce96496944567cf9dbf70939e493801417
Siddharth Shankar <sdshank2@yahoo.com>
2025-03-28 20:12:02 -0400

    Update navbar

--- src/assets/NavBar.jsx
+++ src/assets/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-100 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-green-700 dark:border-gray-700">
                         <li>
-                            <a href="/stemscholars-website/home" className="block py-2 px-3 text-green-700 rounded-sm md:bg-transparent md:hover:text-blue-700 md:dark:hover:text-black md:p-0 dark:text-white" aria-current="page">Home</a>
+                            <a href="/stemscholars-website" className="block py-2 px-3 text-green-700 rounded-sm md:bg-transparent md:hover:text-blue-700 md:dark:hover:text-black md:p-0 dark:text-white" aria-current="page">Home</a>
                         </li>
                         <li>
                             <a href="/stemscholars-website/information" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Information</a>

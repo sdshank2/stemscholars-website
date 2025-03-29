b5d421aef660bc23c5d5c18054f76d378f46cd02
Siddharth Shankar <sdshank2@yahoo.com>
2025-03-28 20:22:28 -0400

    Fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import Home from './Pages/Home'
-import SubmitForm from './Pages/SubmitForm'
-import Information from './Pages/Information'
-import Contact from './Pages/Contact'
+import Home from './assets/Home.jsx'
+import SubmitForm from './pages/SubmitForm.jsx'
+import Information from './pages/Information.jsx'
+import Contact from './pages/Contact.jsx'
 import NavBar from "./assets/NavBar.jsx";
 
 const App = () => {
     let component
     switch (window.location.pathname) {
-        case '/stemscholars-website':
+        case '/stemscholars-website/':
             component = <Home/>
             break
-        case '/stemscholars-website/information':
+        case '/stemscholars-website/information/':
             component = <Information/>
             break
-        case '/stemscholars-website/submitForm':
+        case '/stemscholars-website/submitForm/':
             component = <SubmitForm/>
             break
-        case '/stemscholars-website/contact':
+        case '/stemscholars-website/contact/':
             component = <Contact/>
             break
     }
--- src/assets/NavBar.jsx
+++ src/assets/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav className="sticky top-0 bg-white border-gray-200 dark:bg-green-700 z-50">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/stemscholars-website" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/stemscholars-website/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="GridLink Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GridLink</span>
                 </a>
@@ -17,16 +17,16 @@ const NavBar = () => {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-100 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-green-700 dark:border-gray-700">
                         <li>
-                            <a href="/stemscholars-website" className="block py-2 px-3 text-green-700 rounded-sm md:bg-transparent md:hover:text-blue-700 md:dark:hover:text-black md:p-0 dark:text-white" aria-current="page">Home</a>
+                            <a href="/stemscholars-website/" className="block py-2 px-3 text-green-700 rounded-sm md:bg-transparent md:hover:text-blue-700 md:dark:hover:text-black md:p-0 dark:text-white" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="/stemscholars-website/information" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Information</a>
+                            <a href="/stemscholars-website/information/" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Information</a>
                         </li>
                         <li>
-                            <a href="/stemscholars-website/submitForm" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Submit a Form</a>
+                            <a href="/stemscholars-website/submitForm/" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Submit a Form</a>
                         </li>
                         <li>
-                        <a href="/stemscholars-website/contact" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        <a href="/stemscholars-website/contact/" className="block py-2 px-3 text-green-700 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-black dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                     </li>
                     </ul>
                 </div>

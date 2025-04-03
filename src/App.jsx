1e8af625df422b846d3155ddeb581f808d731998
Siddharth Shankar <sdshank2@yahoo.com>
2025-04-03 18:54:00 -0400

    Implement react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './assets/Home.jsx'
 import SubmitForm from './pages/SubmitForm.jsx'
 import Information from './assets/Information.jsx'
@@ -6,28 +7,19 @@ import Contact from './pages/Contact.jsx'
 import NavBar from "./assets/NavBar.jsx";
 
 const App = () => {
-    let component
-    switch (window.location.pathname) {
-        case '/stemscholars-website/':
-            component = <Home/>
-            break
-        case '/stemscholars-website/information/':
-            component = <Information/>
-            break
-        case '/stemscholars-website/submitForm/':
-            component = <SubmitForm/>
-            break
-        case '/stemscholars-website/contact/':
-            component = <Contact/>
-            break
-    }
     return (
-        <>
+        <Router basename="/stemscholars-website">
             <div className="bg-white dark:bg-black">
                 <NavBar />
-                <div>{component}</div>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/information" element={<Information />} />
+                    <Route path="/submitForm" element={<SubmitForm />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
             </div>
-        </>
+        </Router>
     )
 }
 
